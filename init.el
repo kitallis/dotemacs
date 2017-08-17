5dd1bfe8eac00d07d7c86a7b9e90f4aab8853e59
Kiran Gangadharan <me@kirang.in>
2017-08-17 16:10:37 +0530

    Fix: Add setup for remaining packages

--- init.el
+++ init.el
@@ -3,12 +3,20 @@
 
 (require 'package)
 
-;; Add sources to install packages
+;; Define package repositories
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
+
+;; Load and activate emacs packages. Do this first so that the
+;; packages are loaded before you start trying to modify them.
+;; This also sets the load path.
 (package-initialize)
+
+;; Automatically reload files when they change on disk
+(global-auto-revert-mode 1)
+(setq auto-revert-verbose nil)
 
 
 ;; Install use-package
@@ -18,6 +26,26 @@
 (require 'use-package)
 (setq use-package-verbose t)
 (setq-default use-package-always-ensure t)
+
+
+;; On OS X, an Emacs instance started from the graphical user
+;; interface will have a different environment than a shell in a
+;; terminal window, because OS X does not run a shell during the
+;; login. Obviously this will lead to unexpected results when
+;; calling external utilities like make from Emacs.
+;; This library works around this problem by copying important
+;; environment variables from the user's shell.
+;; https://github.com/purcell/exec-path-from-shell
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize))
+
+
+;; Move custom configuration variables set by Emacs, to a seperate file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+
 
 ;; Install and setup company-mode for autocompletion
 (use-package company
@@ -34,5 +62,75 @@
   (setq company-selection-wrap-around t)
   (setq company-tooltip-align-annotations t)
   (setq company-tooltip-flip-when-above t)
-  (setq company-transformers '(company-sort-by-occurrence)) ; weight by frequency
-  )
+   ; weight by frequency
+  (setq company-transformers '(company-sort-by-occurrence)))
+
+
+;; Better syntax highlighting
+(use-package clojure-mode-extra-font-locking
+  :ensure t)
+
+
+;; Highlight matching parentheses
+(require 'paren)
+(show-paren-mode 1)
+(setq show-paren-delay 1)
+(set-face-background 'show-paren-match (face-background 'default))
+(if (eq (frame-parameter nil 'background-mode) 'dark)
+    (set-face-foreground 'show-paren-match "red")
+  (set-face-foreground 'show-paren-match "black"))
+(set-face-attribute 'show-paren-match nil :weight 'extra-bold)
+
+
+;; Paredit makes it easier to navigate/edit s-expressions as blocks.
+(use-package paredit
+  :ensure t
+  :init
+  (add-hook 'clojure-mode-hook 'enable-paredit-mode)
+  (add-hook 'cider-repl-mode-hook 'enable-paredit-mode))
+
+
+;; To add some colors to those boring parens
+(use-package rainbow-delimiters
+  :ensure t
+  :init (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
+
+;; Cider integrates a Clojure buffer with a REPL
+(use-package cider
+  :ensure t
+  :init
+
+  (setq cider-repl-pop-to-buffer-on-connect t
+        cider-show-error-buffer t
+        cider-auto-select-error-buffer t
+        cider-repl-history-file "~/.emacs.d/cider-history"
+        cider-repl-wrap-history t
+        cider-repl-history-size 100
+        cider-repl-use-clojure-font-lock t
+        cider-docview-fill-column 70
+        cider-stacktrace-fill-column 76
+        ;; Stop error buffer from popping up while working in buffers other than REPL:
+        nrepl-popup-stacktraces nil
+        nrepl-log-messages nil
+        nrepl-hide-special-buffers t
+        cider-repl-use-pretty-printing t
+        cider-repl-result-prefix ";; => ")
+
+  :config
+
+  (add-hook 'cider-mode-hook #'eldoc-mode)
+  (add-hook 'cider-repl-mode-hook #'paredit-mode)
+  (add-hook 'cider-repl-mode-hook #'company-mode)
+  (add-hook 'cider-mode-hook #'company-mode)
+  (add-hook 'cider-mode-hook
+            (local-set-key (kbd "<C-return>") 'cider-eval-defun-at-point)))
+
+
+
+;; Aggressively indents your clojure code
+(use-package aggressive-indent
+  :ensure t
+  :commands (aggressive-indent-mode)
+  :config
+  (add-hook 'clojure-mode-hook 'aggressive-indent-mode))
