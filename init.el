592668575608844c676f9c46d1c160ade20bcce4
Kiran Gangadharan <me@kirang.in>
2018-09-12 13:56:41 +0530

    Bind `s-x` to load `smex`

--- init.el
+++ init.el
@@ -78,8 +78,10 @@
 
 
 ;; M-x with recently/frequently used commands
-(use-package smex)
-(global-set-key (kbd "M-x") 'smex)
+(use-package smex
+  :config
+  (global-set-key (kbd "M-x") 'smex)
+  (global-set-key (kbd "s-x") 'smex))
 
 
 ;; On OS X, an Emacs instance started from the graphical user
