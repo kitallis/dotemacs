20c29c05d858639d9fc9885f49e20636c65dfd22
Akshay Gupta <kitallis@gmail.com>
2021-02-11 18:20:12 +0530

    Update packages and restructure configs

--- init-user.el
+++ init-user.el
@@ -1,66 +1,13 @@
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 
-(set-face-attribute 'default nil
-                    :family "Fira Code"
-                    :height 150
-                    :weight 'normal
-                    :width 'normal)
-
-(setq-default line-spacing 0.3
-              help-window-select t
-              truncate-lines t)
-
-;; workaround for alt not working as meta key
-(setq mac-option-key-is-meta nil
-      mac-command-key-is-meta t
-      mac-command-modifier 'super
-      mac-option-modifier 'meta
-      inhibit-splash-screen t
-      inhibit-startup-message t
-      ring-bell-function 'ignore
-      select-enable-clipboard t
-      save-interprogram-paste-before-kill t
-      backup-inhibited t
-      make-backup-files nil
-      auto-save-default nil
-      vc-handled-backends '(Git)
-      default-file-name-coding-system 'utf-8
-      buffer-file-coding-system 'utf-8
-      org-ellipsis " ▶")
-
-;; (set-default 'cursor-type '(bar . 2))
-(set-default 'cursor-type 'box)
-(blink-cursor-mode 0)
-
-(global-hl-line-mode nil)
-(set-face-attribute hl-line-face nil :underline nil)
-
-;; Rewrite selected text
-(delete-selection-mode 1)
-
-(setq org-todo-keywords
-      '((sequence "TODO" "DOING" "|" "DONE")))
-
-;;Treat hyphens as a word character when transposing words
-(defvar clojure-mode-with-hyphens-as-word-sep-syntax-table
-  (let ((st (make-syntax-table clojure-mode-syntax-table)))
-    (modify-syntax-entry ?- "w" st)
-    st))
-
-(defun transpose-words-with-hyphens (arg)
-  "Treat hyphens as a word character when transposing words"
-  (interactive "*p")
-  (with-syntax-table clojure-mode-with-hyphens-as-word-sep-syntax-table
-    (transpose-words arg)))
-
-(define-key clojure-mode-map (kbd "M-t") 'transpose-words-with-hyphens)
-
 ;; ======
 ;; THEMES
 ;; ======
 
-(use-package monokai-pro-theme)
+(use-package monokai-pro-theme
+  :disabled t)
+
 (use-package moe-theme
   :disabled t
   :config
@@ -68,28 +15,38 @@
         show-paren-style 'parenthesis)
   (moe-theme-set-color 'blue)
   (moe-dark))
-(use-package srcery-theme :disabled t)
-(use-package nord-theme :disabled t)
+(use-package srcery-theme
+  :disabled t)
+
+(use-package nord-theme
+  :disabled t)
+
 (use-package darkokai-theme
-  :disabled t
   :config
   (setq darkokai-mode-line-padding 1))
+
 (use-package doom-themes
   :config
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
   (load-theme 'doom-challenger-deep))
-(use-package abyss-theme :disabled t)
-(use-package gruvbox-theme :disabled t)
-(use-package github-theme :disabled t)
+
+(use-package abyss-theme
+  :disabled t)
+
+(use-package gruvbox-theme
+  :disabled t)
+
 (use-package dracula-theme
   :disabled t
   :config
   (load-theme 'dracula))
+
 (use-package bubbleberry-theme
   :disabled t
   :config
   (load-theme 'bubbleberry))
+
 (use-package solarized-theme
   :init
   :disabled t
@@ -97,231 +54,30 @@
   (setq solarized-use-less-bold t)
   (setq solarized-use-variable-pitch nil)
   (setq x-underline-at-descent-line t)
-
   :config
   (load-theme 'solarized-dark))
-
-;; ==========================
-;; OTHER THIRD PARTY PACKAGES
-;; ==========================
 
 (use-package expand-region
   :config (global-set-key (kbd "C-;") 'er/expand-region))
 
-(use-package ido-vertical-mode
-  :config
-  (ido-vertical-mode 1)
-  (setq ido-use-faces t
-        ido-vertical-show-count t
-        ido-vertical-define-keys 'C-n-and-C-p-only))
-
-(use-package ivy
-  :bind ("s-b". 'ivy-switch-buffer)
-  :config
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-height 10)
-  (setq ivy-count-format "")
-  (setq ivy-count-format "(%d/%d) ")
-
-  ;; Use [Enter] to navigate into the directory, not dired-open it.
-  (define-key ivy-minibuffer-map (kbd "C-m") 'ivy-alt-done))
-
-(use-package wgrep)
-
-(use-package rg
-  :config
-  (setq rg-command-line-flags '("-w"))
-  (setq rg-ignore-case 'smart)
-
-  (rg-define-search kg/grep-vc-or-dir
-    :query ask
-    :format regexp
-    :files "everything"
-    :dir (let ((vc (vc-root-dir)))
-           (if vc
-               vc                         ; search root project dir
-             default-directory))          ; or from the current dir
-    :confirm prefix
-    :flags ("--hidden -g !.git")))
-
-(use-package counsel
-  :after rg
-  :config
-  ;; (global-set-key (kbd "s-g") 'counsel-rg)
-  (global-set-key (kbd "C-x C-r") 'counsel-recentf)
-  (setq counsel-rg-base-command "rg -i -w --no-heading --line-number %s .")
-  (setq recentf-max-saved-items 50)
-  (setq recentf-auto-cleanup (* 24 60 60)))
-
-(use-package swiper
-  :config
-  (global-set-key (kbd "s-f") 'swiper))
-
-(use-package org
-  :config
-  (setq org-directory "~/org-notes"
-        org-default-notes-file (concat org-directory "/notes.txt")
-        org-export-coding-system 'utf-8
-        org-ellipsis " ▼ "
-        org-startup-indented nil
-        org-src-fontify-natively t
-        org-src-preserve-indentation t)
-  (setq org-capture-templates
-        '(("l" "Add a link to the linklog" entry
-           (file+olp+datetree (lambda () (concat org-directory "/linklog.org")))
-           "**** %?")))
-  (add-hook 'org-mode-hook
-            (lambda () (when (fboundp 'org-mac-grab-link)
-                         (load-file "~/.emacs.d/org-mac-link.el")))))
-
-(use-package org-bullets
-  :after org
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-
-(use-package multiple-cursors
-  :config
-  (setq-default mc/edit-lines-empty-lines 'ignore
-                mc/insert-numbers-default 1)
-  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this))
-
-(use-package all-the-icons
-  :disabled t)
-
-(use-package doom-modeline
-  :hook (after-init . doom-modeline-init)
-  :init
-  (set-face-attribute 'mode-line nil :height 140)
-  (set-face-attribute 'mode-line-inactive nil :height 140)
-  (set-face-attribute 'mode-line nil :family "Inconsolata Nerd Font" :height 140)
-  :config
-  (setq doom-modeline-buffer-file-name-style 'relative-from-project
-        doom-modeline-icon (display-graphic-p)
-        doom-modeline-major-mode-color-icon t
-        doom-modeline-major-mode-icon t
-        doom-modeline-minor-modes nil
-        doom-modeline-github nil
-        doom-modeline-version nil
-        doom-modeline-height 10
-        doom-modeline-bar-width 1
-        doom-modeline-buffer-encoding nil
-        doom-modeline-vcs-max-length 50
-        doom-modeline-gnus nil
-        doom-modeline-irc nil
-        doom-modeline-persp-name nil
-        doom-modeline-window-width-limit fill-column))
-
-(use-package undo-tree
-  :bind ("s-Z" . 'undo-tree-redo)
-  :config
-  (global-undo-tree-mode))
-
-(use-package markdown-mode
-  :commands (markdown-mode gfm-mode)
-  :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "pandoc")
-  :config
-  (add-hook 'gfm-mode-hook 'linum-mode)
-  (add-hook 'markdown-mode-hook 'linum-mode))
-
-(use-package smartparens
-  :init
-  (add-hook 'markdown-mode-hook 'smartparens-mode))
-
-(use-package hl-todo
-  :config
-  (global-hl-todo-mode t))
-
-(use-package yaml-mode)
-(use-package json-mode)
-
-(use-package counsel-projectile
-  :config
-  (counsel-projectile-mode))
-
-(use-package elixir-mode)
-
-(use-package rust-mode
-  :config
-  (add-hook 'rust-mode-hook #'flycheck-mode)
-  (add-hook 'rust-mode-hook #'smartparens-mode)
-  (add-hook 'rust-mode-hook
-            (lambda ()
-              (local-set-key (kbd "C-c SPC") #'rust-format-buffer)
-              (local-set-key (kbd "C-c c") #'rust-compile)
-              (local-set-key (kbd "C-c C-t") #'rust-test)
-              (local-set-key (kbd "C-c C-r") #'rust-run)
-              (setq company-minimum-prefix-length 1)))
-
-  (use-package racer
-    :init
-    (setq racer-rust-src-path
-          "/Users/kiran/.rustup/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src")
-    :config
-    (add-hook 'rust-mode-hook #'racer-mode)
-    (add-hook 'racer-mode-hook #'eldoc-mode)
-    (add-hook 'racer-mode-hook #'company-mode)
-    (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-    (define-key rust-mode-map (kbd "C-c C-d") #'racer-describe)
-    (setq company-tooltip-align-annotations t
-          racer-eldoc-timeout 0.5))
-
-  (use-package cargo
-    :config (add-hook 'rust-mode-hook 'cargo-minor-mode))
-
-  (use-package flycheck-rust
-    :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
-
-(use-package fast-scroll
-  :config
-  (fast-scroll-config)
-  (fast-scroll-mode 1))
-
-(use-package flycheck-clj-kondo
-  :config
-  (add-hook 'clojure-mode-hook #'flycheck-mode))
-
-(use-package web-mode
-  :mode (("\\.html?\\'" . web-mode)
-         ("\\.css\\'"   . web-mode)
-         ("\\.json\\'"  . web-mode))
-  :config
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
-  (setq web-mode-css-indent-offset 2))
-
-(use-package k8s-mode
-  :hook (k8s-mode . yas-minor-mode))
-
-(use-package dockerfile-mode
-  :config
-  (require 'dockerfile-mode)
-  (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode)))
-
-(use-package golden-ratio
-  :diminish golden-ratio-mode
-  :init
-  (golden-ratio-mode 1))
-
-(use-package dired-sidebar
-  :commands (dired-sidebar-toggle-sidebar)
-  :config
-  (setq dired-sidebar-theme 'icons)
-  (setq dired-sidebar-use-custom-font t)
-  (setq dired-sidebar-face '(:family "Helvetica" :height 110)))
-
-;; ==============
-;;    EFUNS
-;; ==============
-
 (defun load-local (file)
+  "Load local .el files"
   (load (f-expand file user-emacs-directory)))
 
+
+;; ====
+;; CODE
+;; ====
+
+(load-local "init-code")
+
+
+;; =====
+;; EFUNS
+;; =====
+
 (load-local "init-efuns")
+
 
 ;; ==================
 ;; GLOBAL KEYBINDINGS
@@ -344,5 +100,8 @@
 (global-set-key (kbd "<s-S-return>") 'kg/toggle-maximize-buffer)
 
 
-;; === ============ ADD THEME FOLDER =========================
+;; ================
+;; ADD THEME FOLDER
+;; ================
+
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
--- init.el
+++ init.el
@@ -1,15 +1,11 @@
-;; A minimial setup for Clojurians
-
 ;; Reduce the frequency of garbage collection by making it happen on
 ;; each 50MB of allocated data (the default is on every 0.76MB)
 (setq gc-cons-threshold 50000000
       gc-cons-percentage 0.6)
 
-
 ;; Automatically reload files when they change on disk
 (global-auto-revert-mode 1)
 (setq auto-revert-verbose nil)
-
 
 ;; Bootstrap configuration for straight.el
 (setq straight-use-package-by-default t)
@@ -28,24 +24,68 @@
 
 (straight-use-package 'use-package)
 
-
 ;; Move custom configuration variables set by Emacs, to a seperate file
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
 
 
-;;
+;; ==========
 ;; APPEARANCE
-;;
+;; ==========
 
+;; Font
+(set-face-attribute 'default nil
+                    :family "Fira Code"
+                    :height 150
+                    :weight 'normal
+                    :width 'normal)
+
+;; Type
+(setq-default line-spacing 0.3
+              help-window-select t
+              ;; Hide cursor in inactive windows
+              cursor-in-non-selected-windows nil
+              truncate-lines t)
+
+
+;; Workaround for alt not working as meta key
+(setq mac-option-key-is-meta nil
+      mac-command-key-is-meta t
+      mac-command-modifier 'super
+      mac-option-modifier 'meta
+      inhibit-splash-screen t
+      inhibit-startup-message t
+      ring-bell-function 'ignore
+      select-enable-clipboard t
+      save-interprogram-paste-before-kill t
+      backup-inhibited t
+      make-backup-files nil
+      auto-save-default nil
+      vc-handled-backends '(Git)
+      default-file-name-coding-system 'utf-8
+      buffer-file-coding-system 'utf-8
+      org-ellipsis " ▶")
+
+;; Cursor style
+(set-default 'cursor-type '(bar . 2)) ;; or (set-default 'cursor-type 'box)
+
+;; Toggle cursor blinking at rest
+(blink-cursor-mode 1)
+
+;; TODO
+(global-hl-line-mode nil)
+
+;; TODO
+(set-face-attribute hl-line-face nil :underline nil)
+
+;; Rewrite selected text
+(delete-selection-mode 1)
 
 ;; don't show the tool bar
 (tool-bar-mode -1)
 
-
 ;; don't show the scroll bar
 (scroll-bar-mode -1)
-
 
 ;; Make the title bar blend with the background color
 ;; Set the appearance to light/dark depending on your theme
@@ -54,15 +94,6 @@
 (add-to-list 'default-frame-alist
              '(ns-appearance . dark))
 
-
-;; Hide cursor in inactive windows
-(setq-default cursor-in-non-selected-windows nil)
-
-
-;; ;; Set default line spacing
-;; (setq-default line-spacing 0.2)
-
-
 ;; Show full file path in the title bar
 (setq
  frame-title-format
@@ -70,26 +101,18 @@
               (abbreviate-file-name (buffer-file-name))
             "%b"))))
 
-
-;; ;; Set font and line spacing
-;; (set-face-attribute 'default nil :font "Inconsolata LGC 15")
-;; (setq-default line-spacing 0.3)
-
-
 ;; Line numbers
 ;; Add some padding when displaying line numbers
 (setq linum-format "%5d ")
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
 
-;;
+;; =============
 ;; SANE DEFAULTS
-;;
-
+;; =============
 
 ;; Use y/n instead of full yes/no for confirmation messages
 (fset 'yes-or-no-p 'y-or-n-p)
-
 
 ;; Use spaces instead of tabs
 (setq tab-width 2)
@@ -101,28 +124,22 @@
 (setq-default tab-width 2)
 (setq-default c-basic-indent 2)
 
-
 ;; Type over selected text
 (delete-selection-mode 1)
 
-
 ;; Kill whole line
 (global-set-key (kbd "s-<backspace>") 'kill-whole-line)
-
 
 ;; Use Cmd for movement
 (global-set-key (kbd "s-<right>") (kbd "C-e"))  ;; End of line
 (global-set-key (kbd "s-<left>") (kbd "C-a"))   ;; Beginning of line
 
-
 ;; Kills the current buffer without displaying the annoying menu.
 ;; A confirmation will be asked for, if the buffer has been modified
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
 
-
 ;; Remove trailing whitespace before saving
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-
 
 ;; On OS X, an Emacs instance started from the graphical user
 ;; interface will have a different environment than a shell in a
@@ -136,13 +153,11 @@
   :config
   (exec-path-from-shell-initialize))
 
-
 ;; M-x with recently/frequently used commands
 (use-package smex
   :config
   (global-set-key (kbd "M-x") 'smex)
   (global-set-key (kbd "s-x") 'smex))
-
 
 ;; Install and setup company-mode for autocompletion
 (use-package company
@@ -168,7 +183,6 @@
 ;; Better syntax highlighting
 (use-package clojure-mode-extra-font-locking)
 
-
 ;; Highlight matching parentheses
 (require 'paren)
 (show-paren-mode 1)
@@ -179,11 +193,9 @@
   (set-face-foreground 'show-paren-match "black"))
 (set-face-attribute 'show-paren-match nil :weight 'extra-bold)
 
-
 ;; Add ability to shift between buffers using shift+arrow keys.
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
-
 
 ;; Paredit makes it easier to navigate/edit s-expressions as blocks.
 (use-package paredit
@@ -191,7 +203,6 @@
   (add-hook 'clojure-mode-hook 'enable-paredit-mode)
   (add-hook 'cider-repl-mode-hook 'enable-paredit-mode)
   (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode))
-
 
 ;; To add some colors to those boring parens
 (use-package rainbow-delimiters
@@ -235,7 +246,6 @@
               (local-set-key (kbd "C-c C-n") 'cider-eval-buffer)
               (local-set-key (kbd "C-x C-i") 'cider-inspect-last-sexp))))
 
-
 ;; Adds some niceties/refactoring support
 (use-package clj-refactor
   :config
@@ -244,20 +254,17 @@
             (lambda ()
               (clj-refactor-mode 1))))
 
-
 (use-package flycheck-clj-kondo
   :config
   (remove-hook 'clojure-mode-hook
                (lambda ()
                  (require 'flycheck-clj-kondo))))
 
-
 ;; Aggressively indents your clojure code
 (use-package aggressive-indent
   :commands (aggressive-indent-mode)
   :config
   (add-hook 'clojure-mode-hook 'aggressive-indent-mode))
-
 
 ;; Operate (list, search, replace....) on files at a project level.
 (use-package projectile
@@ -277,6 +284,59 @@
                 ;; Show project (if any) name in modeline
                 projectile-mode-line '(:eval (projectile-project-name))))
 
+;; TODO
+(use-package ido-vertical-mode
+  :config
+  (ido-vertical-mode 1)
+  (setq ido-use-faces t
+        ido-vertical-show-count t
+        ido-vertical-define-keys 'C-n-C-p-up-down-left-right))
+
+;; TODO
+(use-package ivy
+  :bind ("s-b". 'ivy-switch-buffer)
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-height 10)
+  (setq ivy-count-format "")
+  (setq ivy-count-format "(%d/%d) ")
+
+  ;; Use [Enter] to navigate into the directory, not dired-open it.
+  (define-key ivy-minibuffer-map (kbd "C-m") 'ivy-alt-done))
+
+;; Apply grep changes to file without sed etc.
+(use-package wgrep)
+
+;; Replace grep/ag with ripgrep
+(use-package rg
+  :config
+  (setq rg-command-line-flags '("-w"))
+  (setq rg-ignore-case 'smart)
+
+  (rg-define-search kg/grep-vc-or-dir
+    :query ask
+    :format regexp
+    :files "everything"
+    :dir (let ((vc (vc-root-dir)))
+           (if vc
+               vc                         ; search root project dir
+             default-directory))          ; or from the current dir
+    :confirm prefix
+    :flags ("--hidden -g !.git")))
+
+(use-package counsel
+  :after rg
+  :config
+  ;; (global-set-key (kbd "s-g") 'counsel-rg)
+  (global-set-key (kbd "C-x C-r") 'counsel-recentf)
+  (setq counsel-rg-base-command "rg -i -w --no-heading --line-number %s .")
+  (setq recentf-max-saved-items 50)
+  (setq recentf-auto-cleanup (* 24 60 60)))
+
+(use-package swiper
+  :config
+  (global-set-key (kbd "s-f") 'swiper))
 
 ;; Magit: The only git interface you'll ever need
 (use-package magit
@@ -285,19 +345,111 @@
   (setq magit-set-upstream-on-push 'askifnotset))
 
 
+;; Org mode
+(use-package org
+  :config
+  (setq org-directory "~/org-notes"
+        org-default-notes-file (concat org-directory "/notes.txt")
+        org-export-coding-system 'utf-8
+        org-ellipsis " ▼ "
+        org-startup-indented nil
+        org-src-fontify-natively t
+        org-src-preserve-indentation t)
+  (setq org-capture-templates
+        '(("l" "Add a link to the linklog" entry
+           (file+olp+datetree (lambda () (concat org-directory "/linklog.org")))
+           "**** %?")))
+  (add-hook 'org-mode-hook
+            (lambda () (when (fboundp 'org-mac-grab-link)
+                         (load-file "~/.emacs.d/org-mac-link.el")))))
+
+(setq org-todo-keywords
+      '((sequence "TODO" "DOING" "|" "DONE")))
+
+(use-package org-bullets
+  :after org
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(use-package all-the-icons)
+
+(use-package multiple-cursors
+  :config
+  (setq-default mc/edit-lines-empty-lines 'ignore
+                mc/insert-numbers-default 1)
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this))
+
+(use-package doom-modeline
+  :hook (after-init . doom-modeline-init)
+  :init
+  (set-face-attribute 'mode-line nil :height 140)
+  (set-face-attribute 'mode-line-inactive nil :height 140)
+  (set-face-attribute 'mode-line nil :family "Inconsolata Nerd Font" :height 140)
+  :config
+  (setq doom-modeline-buffer-file-name-style 'relative-from-project
+        doom-modeline-icon (display-graphic-p)
+        doom-modeline-major-mode-color-icon t
+        doom-modeline-major-mode-icon t
+        doom-modeline-minor-modes nil
+        doom-modeline-github nil
+        doom-modeline-version nil
+        doom-modeline-height 10
+        doom-modeline-bar-width 1
+        doom-modeline-buffer-encoding nil
+        doom-modeline-vcs-max-length 50
+        doom-modeline-gnus nil
+        doom-modeline-irc nil
+        doom-modeline-persp-name nil
+        doom-modeline-window-width-limit fill-column))
+
+(use-package undo-tree
+  :bind ("s-Z" . 'undo-tree-redo)
+  :config
+  (global-undo-tree-mode))
+
+(use-package hl-todo
+  :config (global-hl-todo-mode t))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package fast-scroll
+  :config
+  (fast-scroll-config)
+  (fast-scroll-mode 1))
+
+(use-package golden-ratio
+  :diminish golden-ratio-mode
+  :init
+  (golden-ratio-mode 1))
+
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+  (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-face '(:family "Menlo"))
+  (setq dired-sidebar-use-custom-font t))
+
+
+;; ===================
 ;; User customizations
+;; ===================
+
 ;; Add your customizations to `init-user.el`
 (when (file-exists-p "~/.emacs.d/init-user.el")
   (setq user-custom-file "~/.emacs.d/init-user.el")
   (load user-custom-file)
   (put 'downcase-region 'disabled nil))
 (put 'narrow-to-region 'disabled nil)
-
-
-(require 'server)
-(unless (server-running-p)
-  (server-start))
-
 
 ;; Reset GC to reasonable defaults
 (add-hook 'emacs-startup-hook
